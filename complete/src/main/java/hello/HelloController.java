ebd458261f52d2f11b6658c225d946c8240e12c1
Mike Dame <damemi@gmail.com>
2015-06-29 23:24:58 +0000

    Turning on and off

--- complete/src/main/java/hello/HelloController.java
+++ complete/src/main/java/hello/HelloController.java
@@ -7,14 +7,35 @@ import java.io.IOException;
 @RestController
 public class HelloController {
     
-    @RequestMapping("/greeting")
-    public String index() {
+    @RequestMapping("/on")
+    public String on() {
 	try {
-	    Process p = Runtime.getRuntime().exec(new String[]{"/usr/local/bin/gpio write 4 on"});
+	    Process p = Runtime.getRuntime().exec("/usr/local/bin/gpio write 4 on");
 	} catch(IOException e) {
 	    e.printStackTrace();
 	}
-        return "Greetings from Spring Boot!";
+        return "Turning on. <a href=\"/off\">Turn off</a>";
     }
+
+    @RequestMapping("/off")
+    public String off() {
+	try {
+	    Process p = Runtime.getRuntime().exec("/usr/local/bin/gpio write 4 off");
+	} catch(IOException e) {
+	    e.printStackTrace();
+	}
+        return "Turning off. <a href=\"/on\">Turn on</a>";
+    }
+
+    @RequestMapping("/mode")
+    public String mode() {
+	try {
+	    Process p = Runtime.getRuntime().exec("/usr/local/bin/gpio mode 4 out");
+	} catch(IOException e) {
+	    e.printStackTrace();
+	}
+        return "Turning mode to out";
+    }
+
     
 }
