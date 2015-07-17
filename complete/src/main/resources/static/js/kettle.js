71f1bd6218c22288da381c624afdad525eee3bbe
Mike Dame <damemi@gmail.com>
2015-07-16 20:44:48 -0400

    Actually adding angular, bootstrap

---
+++ complete/src/main/resources/static/js/kettle.js
@@ -0,0 +1,13 @@
+angular.module('kettleApp',[])
+    .controller('kettleController', function($scope, $http) {
+	$scope.power = "off";
+	$scope.turnOn = function() {
+	    $scope.power = "on";
+	    $http.get('/on');
+	}
+
+	$scope.turnOff = function() {
+	    $scope.power = "off";
+	    $http.get('/off');
+	}
+    });
