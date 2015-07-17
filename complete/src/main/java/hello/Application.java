aa4760579b959b6b87907c9a813e2413244cb716
Mike Dame <damemi@gmail.com>
2015-07-17 01:17:16 +0000

    Startup settings

--- complete/src/main/java/hello/Application.java
+++ complete/src/main/java/hello/Application.java
@@ -8,6 +8,7 @@ import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import java.io.IOException;
 
 @Configuration
 @ComponentScan
@@ -16,9 +17,15 @@ public class Application {
     
     public static void main(String[] args) {
         ApplicationContext ctx = SpringApplication.run(Application.class, args);
+
+	try {
+	    Process p = Runtime.getRuntime().exec("/usr/local/bin/gpio mode 4 out");
+	    System.out.println("[INFO] Set gpio mode to out\n");
+	} catch(IOException e) {
+	    e.printStackTrace();
+	}
         
-        System.out.println("Kettle is up and running!\n");
-        
+        System.out.println("Kettle is up and running!\n");        
 	
     }
 
