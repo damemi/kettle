9bdfaa6785dbee7b301580cd142b114142e7cf48
Mike Dame <damemi@gmail.com>
2015-07-16 20:21:21 -0400

    Add on/off buttons and Angular

--- complete/src/main/java/hello/Application.java
+++ complete/src/main/java/hello/Application.java
@@ -17,13 +17,9 @@ public class Application {
     public static void main(String[] args) {
         ApplicationContext ctx = SpringApplication.run(Application.class, args);
         
-        System.out.println("Let's inspect the beans provided by Spring Boot:");
+        System.out.println("Kettle is up and running!\n");
         
-        String[] beanNames = ctx.getBeanDefinitionNames();
-        Arrays.sort(beanNames);
-        for (String beanName : beanNames) {
-            System.out.println(beanName);
-        }
+	
     }
 
 }
