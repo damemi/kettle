16b3b64fe195506ed183c32177edda30898ab356
Mike Dame <damemi@gmail.com>
2015-07-20 18:39:12 -0400

    Adding classes

---
+++ complete/src/main/java/hello/AlarmJob.java
@@ -0,0 +1,16 @@
+package hello;
+ 
+import org.quartz.Job;
+import org.quartz.JobExecutionContext;
+import org.quartz.JobExecutionException;
+ 
+public class AlarmJob implements Job
+{
+    public void execute(JobExecutionContext context)
+	throws JobExecutionException {
+ 
+	System.out.println("Hello Quartz!");
+ 
+    }
+ 
+}
