ed2767e9ab1652ec4d8dae5dcde796bf14dffeb8
Mike Dame <damemi@gmail.com>
2015-07-20 18:38:52 -0400

    Creating jobs like obama

--- complete/src/main/java/hello/HelloController.java
+++ complete/src/main/java/hello/HelloController.java
@@ -2,11 +2,58 @@ package hello;
 
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.ResponseBody;
 import java.io.IOException;
+import hello.AlarmJob;
+import org.quartz.JobBuilder;
+import org.quartz.JobDetail;
+import org.quartz.Scheduler;
+import org.quartz.SchedulerException;
+import org.quartz.SchedulerFactory;
+//import org.quartz.SimpleScheduleBuilder;
+import org.quartz.Trigger;
+import org.quartz.SimpleTrigger;
+//import org.quartz.TriggerBuilder;
+import org.quartz.impl.StdSchedulerFactory;
+import static org.quartz.TriggerBuilder.*;
+import static org.quartz.SimpleScheduleBuilder.*;
+import static org.quartz.DateBuilder.*;
+import java.util.*;
+
 
 @RestController
 public class HelloController {
     
+    @RequestMapping(value = "/setAlarm/{hour}/{minute}/{ampm}")
+	@ResponseBody
+	public String setAlarm(@PathVariable("hour") String hour,
+			       @PathVariable("minute") String minute,
+			       @PathVariable("ampm") String ampm) {
+
+	try {
+	    long startTime = System.currentTimeMillis() + 10000L;
+	    Date myStart = new Date(startTime);
+	    JobDetail job = JobBuilder.newJob(AlarmJob.class)
+		.withIdentity("alarmJob")
+		.build();
+	
+	    SimpleTrigger trigger = (SimpleTrigger) newTrigger() 
+		.withIdentity("trigger1")
+		.startAt(myStart)
+		.forJob("alarmJob")
+		.build();
+
+	    SchedulerFactory schFactory = new StdSchedulerFactory();
+	    Scheduler sch = schFactory.getScheduler();
+	    sch.start();
+	    sch.scheduleJob(job, trigger);
+	} catch (SchedulerException e) {
+	    e.printStackTrace();
+	}
+	return "True";
+    }
+
     @RequestMapping("/on")
     public String on() {
 	try {
--- complete/src/main/resources/static/js/kettle.js
+++ complete/src/main/resources/static/js/kettle.js
@@ -2,6 +2,17 @@ angular.module('kettleApp',[])
     .controller('kettleController', function($scope, $http) {
 	$scope.power = "off";
 
+	$scope.getNumber = function(num) {
+	    var arr = [];
+	    for(var i=0; i<num; i++) {
+		if(i < 10)
+		    arr.push("0"+i);
+		else
+		    arr.push(i);
+	    }
+	    return arr;
+	}
+
 	$scope.turnOn = function() {
 	    $scope.power = "on";
 	    $http.get('/on');
